0735692c185ae3c741414a2c7153b5fdfc065e9f
yutanohara <yu0812zzz@gmail.com>
2018-08-01 22:30:08 +0900

    routesの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get '/listings/registar' => 'listings#registar'
-  get '/listings/:id/edit1' => 'listings#edit1'
-  get '/listings/options' => 'listings#options'
-  get '/listings/setting' => 'listings#setting'
-  get '/listings/:id/setting1' => 'listings#setting1'
-  get '/listings/tag' => 'listings#tag'
-  resources :listings
-  get 'users/index'
-  get 'users/show'
+  resources :listings do
+    collection do
+      get 'registar'
+      get 'options'
+      get 'setting'
+      get 'tag'
+    end
+    member do
+      get 'edit1'
+      get 'setting1'
+    end
+  end
 
   root to: 'listings#tag'
 
