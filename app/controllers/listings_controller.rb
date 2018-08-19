b47f1cc46b9e47be465e9c6a40be4fcd9b69e0eb
yutanohara <yu0812zzz@gmail.com>
2018-08-19 11:49:43 +0900

    data.jsを観れるようにした

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[tag index]
+  before_action :authenticate_user!, except: %i[tag index data]
   protect_from_forgery except: [:data]
 
   # GET /listings
