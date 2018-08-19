a61d525e062118bfc23bd125c2fa9aa5224ded68
yutanohara <yu0812zzz@gmail.com>
2018-08-19 13:08:00 +0900

    fix config

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,21 @@ Bundler.require(*Rails.groups)
 module Listingkun
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    config.i18n.default_locale = :ja
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Credentials' => 'true',
-      'Access-Control-Allow-Origin' => 'https://listing-kahen.herokuapp.com',
-      'Access-Control-Request-Method' => '*'
-    }
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
+
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Credentials' => 'true',
+    #   'Access-Control-Allow-Origin' => 'https://listing-kahen.herokuapp.com',
+    #   'Access-Control-Request-Method' => '*'
+    # }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
